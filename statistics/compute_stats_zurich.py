04ef8b6e550698db5a4709fbd8854c1b603ed281
valosekj <jan.valosek@upol.cz>
2023-05-30 15:51:12 -0400

    Print only 2 Candidate predictors perline

--- statistics/compute_stats_zurich.py
+++ statistics/compute_stats_zurich.py
@@ -364,10 +364,10 @@ def compute_stepwise(y, x, threshold_in, threshold_out, method):
         included: list of selected predictor
 
     """
-    # Print columns names for x (print list with 3 elements per line --> better for copy-paste)
+    # Print columns names for x (print list with 2 elements per line --> better for copy-paste)
     print('Candidate predictors: ')
-    for i in range(0, len(list(x.columns)), 3):
-        print(list(x.columns)[i:i+3])
+    for i in range(0, len(list(x.columns)), 2):
+        print(list(x.columns)[i:i+2])
     included = []  # Initialize a list for included predictors in the model
     while True:
         changed = False
