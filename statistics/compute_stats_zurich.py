0472ae1378bc126b1cea605b2d0997a0ad748b7b
valosekj <jan.valosek@upol.cz>
2023-08-28 18:43:56 -0400

    Move SmartFormatter class to utils.py

--- statistics/compute_stats_zurich.py
+++ statistics/compute_stats_zurich.py
@@ -61,15 +61,6 @@ DICT_DISC_LABELS = {
 }
 
 
-class SmartFormatter(argparse.HelpFormatter):
-
-    def _split_lines(self, text, width):
-        if text.startswith('R|'):
-            return text[2:].splitlines()
-        # this is the RawTextHelpFormatter._split_lines
-        return argparse.HelpFormatter._split_lines(self, text, width)
-
-
 def get_parser():
     parser = argparse.ArgumentParser(
         description="Computes statistics and generates figures for the dcm-zurich dataset.",
