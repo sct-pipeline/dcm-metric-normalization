83aac0ead26392837e8a3c91a06dd8a26ed3e4b7
valosekj <jan.valosek@upol.cz>
2025-07-29 15:23:18 +0200

    add PAM50 normalization

--- hog_angle/compute_diameters.sh
+++ hog_angle/compute_diameters.sh
@@ -136,8 +136,13 @@ fi
 # Generate labeled segmentation using init disc labels
 sct_label_vertebrae -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -discfile ${FILEDISCS}.nii.gz -c t2 -qc ${PATH_QC} -qc-subject ${file}
 
-sct_process_segmentation -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -vertfile ${FILESEG}_labeled.nii.gz -perslice 1 -o ${PATH_RESULTS}/${file}_metrics_PAM50_angle_corr0.csv -qc qc -angle-corr 0
-sct_process_segmentation -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -vertfile ${FILESEG}_labeled.nii.gz -perslice 1 -o ${PATH_RESULTS}/${file}_metrics_PAM50_angle_corr1.csv -qc qc -angle-corr 1
+# Normalize to PAM50, with and without the angle correction
+sct_process_segmentation -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -vertfile ${FILESEG}_labeled.nii.gz -perslice 1 -normalize-PAM50 1 -o ${PATH_RESULTS}/${file}_metrics_PAM50_angle_corr0.csv -qc ${PATH_QC} -angle-corr 0
+sct_process_segmentation -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -vertfile ${FILESEG}_labeled.nii.gz -perslice 1 -normalize-PAM50 1 -o ${PATH_RESULTS}/${file}_metrics_PAM50_angle_corr1.csv -qc ${PATH_QC} -angle-corr 1
+
+# No normalization to PAM50, with and without the angle correction
+sct_process_segmentation -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -vertfile ${FILESEG}_labeled.nii.gz -perslice 1 -o ${PATH_RESULTS}/${file}_metrics_angle_corr0.csv -qc ${PATH_QC} -angle-corr 0
+sct_process_segmentation -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -vertfile ${FILESEG}_labeled.nii.gz -perslice 1 -o ${PATH_RESULTS}/${file}_metrics_angle_corr1.csv -qc ${PATH_QC} -angle-corr 1
 
 # Create figures folder under ${PATH_RESULTS} if it does not exist
 mkdir -p ${PATH_RESULTS}/figures
