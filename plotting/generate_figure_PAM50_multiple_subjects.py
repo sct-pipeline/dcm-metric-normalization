1db681e7f58be9b6b9c90bf8265201b567a3e5b0
valosekj <jan.valosek@upol.cz>
2025-11-02 09:41:38 +0100

    'total_mjoa' --> 'total_mjoa_bl'

--- plotting/generate_figure_PAM50_multiple_subjects.py
+++ plotting/generate_figure_PAM50_multiple_subjects.py
@@ -126,7 +126,7 @@ def get_parser():
     parser.add_argument('-participants-file', required=False, type=str,
                         help="Path to the patients' participants.tsv file containing maximum_stenosis or myelopathy data for stratification.")
     parser.add_argument('-clinical-file', required=False, type=str,
-                        help="Excel file with clinical scores (must contain 'total_mjoa' column)")
+                        help="Excel file with clinical scores (must contain 'total_mjoa_bl' column)")
     parser.add_argument('-stratify', required=False, type=str,
                         choices=['mcl', 'myelopathy', 'mjoa', 'therapeutic_decision', 'age', 'None'],
                         help="Stratification method:"
@@ -264,7 +264,7 @@ def read_csv_file(csv_file, participants_file=None, clinical_file=None, stratify
         clinical Excel file with mJOA scores (if provided).
     :param csv_file: input CSV file path
     :param participants_file: path to participants.tsv file with stratification data
-    :param clinical_file: path to Excel file with clinical scores (must contain 'total_mjoa' column)
+    :param clinical_file: path to Excel file with clinical scores (must contain 'total_mjoa_bl' column)
     :param stratify_type: type of stratification ('mcl' or 'myelopathy')
     :return: pandas dataframe with additional columns participant_id, session_id, MEAN(compression_ratio), and optionally stratification data
     """
@@ -339,22 +339,22 @@ def read_csv_file(csv_file, participants_file=None, clinical_file=None, stratify
             sys.exit(f"Warning: Participants file not found: {participants_file}")
     elif stratify_type == 'mjoa':
         if clinical_file and os.path.isfile(clinical_file):
-            df_clinical = pd.read_excel(clinical_file, usecols=['record_id', 'total_mjoa'])
+            df_clinical = pd.read_excel(clinical_file, usecols=['record_id', 'total_mjoa_bl'])
             # Format record_id to match participant_id format (e.g., `1` to `sub-001`)
             df_clinical['participant_id'] = df_clinical['record_id'].apply(lambda x: f'sub-{int(x):03d}')
             # Drop record_id column
             df_clinical = df_clinical.drop(columns=['record_id'])
             # Stratify mJOA
-            df_clinical['mJOA_severity'] = df_clinical['total_mjoa'].apply(_stratify_mjoa)
+            df_clinical['mJOA_severity'] = df_clinical['total_mjoa_bl'].apply(_stratify_mjoa)
 
-            if 'total_mjoa' in df_clinical.columns:
+            if 'total_mjoa_bl' in df_clinical.columns:
                 # Merge mJOA data
                 subjects_df = subjects_df.merge(
                     df_clinical[['participant_id', 'mJOA_severity']],
                     on='participant_id', how='left'
                 )
             else:
-                sys.exit("Warning: 'total_mjoa' column not found in clinical file")
+                sys.exit("Warning: 'total_mjoa_bl' column not found in clinical file")
         else:
             sys.exit(f"Warning: Clinical file not found: {clinical_file}")
     elif stratify_type == 'age':
