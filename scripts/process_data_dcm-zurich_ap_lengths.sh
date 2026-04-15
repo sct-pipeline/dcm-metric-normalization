4be0ced049f9dd0209eea60908c58af49b0a3ab9
valosekj <jan.valosek@upol.cz>
2026-04-15 14:32:19 +0200

    rename config

--- scripts/process_data_dcm-zurich_ap_lengths.sh
+++ scripts/process_data_dcm-zurich_ap_lengths.sh
@@ -11,7 +11,7 @@
 #      and appends results to T2w_ax_cord_metrics_perlevel.csv
 #
 # Usage to exclude specific subjects:
-#     sct_run_batch -c config_process_data_dcm-zurich_ap_lengths.json -exclude-yml exclude_dcm-zurich.yml
+#     sct_run_batch -c config_process_data_dcm-zurich_part01_ap_lengths.json -exclude-yml exclude_dcm-zurich.yml
 #
 # Example YAML exclude file (included in this repo) to exclude some subjects/sessions (note that YAML supports comments):
 #     t2_ax:
